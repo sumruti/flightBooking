ee5b35ae60d96d99df0965ff1719441e10328154
sumruti <46131919+sumruti@users.noreply.github.com>
2019-01-01 21:32:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, 'Dist')));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/Dist/index.html');
+  response.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, error => (
