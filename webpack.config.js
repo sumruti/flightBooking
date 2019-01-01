1b47cff1a2c3c131a96456bad02f736a4c17dc6a
sumruti <46131919+sumruti@users.noreply.github.com>
2019-01-01 22:04:58 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
   ],
   module: {
     loaders: [
-      //{ test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
     ]
   },
