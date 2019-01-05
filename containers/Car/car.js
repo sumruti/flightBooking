8197ebe3813948a7805bef94aa3e49000be962c0
sumruti <46131919+sumruti@users.noreply.github.com>
2019-01-05 14:21:25 +0530

    update

--- containers/Car/car.js
+++ containers/Car/car.js
@@ -22,6 +22,36 @@ class car extends Component {
       flexDirectionNav: false,
       controlNav: false,
     });
+    $("#owl-car-offers").owlCarousel({
+    items : 3,
+    itemsCustom : false,
+    itemsDesktop : [1199,2],
+    itemsDesktopSmall : [991,2],
+    itemsTablet: [768,2],
+    itemsTabletSmall: [767,1],
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false,
+  
+    //Autoplay
+    autoPlay : true,
+    stopOnHover : true,
+   
+    // Navigation
+    navigation : true,
+    navigationText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    rewindNav : true,
+    scrollPerPage : false,
+   
+    //Pagination
+    pagination : false,
+    paginationNumbers: false,
+   
+    // Responsive 
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,    
+  });
   }
 render () {
 
