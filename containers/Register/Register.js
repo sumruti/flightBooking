55679cf3d559183a361fea4f8b236b4bd8b17ece
sumruti <46131919+sumruti@users.noreply.github.com>
2019-01-07 22:23:25 +0530

    update

--- containers/Register/Register.js
+++ containers/Register/Register.js
@@ -97,7 +97,7 @@ render () {
                 <div className="row">
                   <div className="col-sm-12">
                     <div className="full-page-title">
-                      <h3 className="company-name"><span><i className="fa fa-plane" />UBRHUGTUR</span></h3>
+                      <h3 className="company-name"><span>UBRHUG</span>TUR</h3>
 {/*                      <p>Lorem ipsum dolor sit amet, conse adipiscing elit. Curabitur metus felis, venenatis eu ultricies vel, vehicula eu urna. Phasellus eget augue id est fringilla feugiat id a tellus. Sed hendrerit quam sed ante euismod posuere ultricies. </p>
                     */}                    </div>{/* end full-page-title */}
                     <div className="custom-form custom-form-fields">
